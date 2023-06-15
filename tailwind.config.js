8a7c8f9b779f9ec5d675d01d0c9bc47b74030467
gsngdavid <gsngdavid@gmail.com>
2023-06-15 07:13:24 +0200

    First part

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./build/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        "primary": "#171a20",
+        "light-dark": "rgba(0, 0, 0, 0.15)",
+        "graysh-light": "#e2e3e3",
+      },
+      backgroundImage: {
+        "top-ad": "radial-gradient(54.97% 272.54% at 27.36% -134.72%,rgba(108,148,255,.92) 0,rgba(0,0,0,.92) 100%)",
+        "models": "url('../images/models.jpeg')",
+        "blue-car": "url('../images/blue car.avif')",
+      },
+      boxShadow: {
+        "bottom-lg": "box-shadow: inset 0 0 500px 8px #000"
+      }
+    },
+  },
+  plugins: [],
+}
+
